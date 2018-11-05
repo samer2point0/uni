762df4d69a675a1e2d67f7808cdf4315a33942c4
Samer Mohamed <Samirsuraj@live.com>
2018-11-05 04:18:49 +0200

    finished coding of NLP exc1

--- NLP/exc1/exc.py
+++ NLP/exc1/exc.py
@@ -3,6 +3,7 @@ import jsonlines
 import nltk
 import os
 import collections
+import math
 nltk.download()
 
 with open('./signal-news1/opinion-lexicon-English/positive-words.txt', 'r') as pWF:
@@ -43,47 +44,128 @@ def pVn(pnCount,artText):
         pnCount['pArticles']=pnCount['pArticles']+1
     elif(NWs>PWs):
         pnCount['nArticles']=pnCount['nArticles']+1
-    return pnCount
 
+def topTrigram(trigrams, bigram):
+    triList=[]
+    tempTri=trigrams
+    while True:
+        if len(tempTri)>1 and tempTri[int(len(tempTri)/2)][0][:2] < bigram:
+            tempTri=tempTri[int(len(tempTri)/2):]
+        elif len(tempTri)>1 and tempTri[int(len(tempTri)/2)][0][:2] > bigram:
+            tempTri=tempTri[:int(len(tempTri)/2)]
+        else:
+            if len(tempTri)<1 or tempTri[int(len(tempTri)/2)][0][:2] != bigram:
+                return None
 
-pnCount={'pArticles':0,'nArticles':0, 'nCount':0, 'pCount':0}
-T,V=[],[]
-breakFlag=0
-with jsonlines.open('./signal-news1/signal-news1.jsonl', 'r') as reader:
-    i=0
-    for line in reader:
-        i=i+1
-        text=line['content'].lower()
-
-        #preocess text and lemmatize it and add it ot list
-        proctext=prep(text).split()
-        del text
-        lemT=lemmatize(proctext)
-        del proctext
-
-        print(i)
-        T.extend(lemT)
-        if(i==160):
-            breakFlag=len(T)
-
-        tempV=collections.Counter(lemT)
-        del lemT
-        pnCount=pVn(pnCount,tempV)
-
-        if(i==200):
+            i,j=0,1
+            while int(len(tempTri)/2)-i>0 and tempTri[int(len(tempTri)/2)-i][0][:2]==bigram:
+                triList.insert(0, tempTri[int(len(tempTri)/2)-i])
+                i=i+1
+            while int(len(tempTri)/2)+j<len(tempTri) and tempTri[int(len(tempTri)/2)+j][0][:2]==bigram:
+                triList.append(tempTri[int(len(tempTri)/2)+j])
+                j=j+1
             break
 
-vCount=len(set(T))
-tCount=len(T)
-print(vCount, tCount)
-print(pnCount)
+    topTri=max(triList, key=lambda x: x[1])[0]
+    return topTri
 
-V1=collections.Counter(T[:breakFlag])
-tri= collections.Counter(nltk.util.ngrams(T,3))
+def finishSentence(trigrams, ngram, n):
+    bigram=tuple(ngram.split()[-2:])
+    topTri=topTrigram(trigrams,bigram)
+    if not topTri:
+        return ngram
+    topWord=topTri[-1]
+    ngram=ngram+' '+topWord
 
-topTri=sorted(tri.items(), key=lambda x: x[1])
-print(topTri[-25:])
+    if len(ngram.split())<n:
+        sentence=finishSentence(trigrams, ngram, n)
+    else:
+        sentence=ngram
 
-#sortedTri1=removeTri2(tri,tri2)
+    return sentence
 
-V2=set(T2)
+
+def preplexity(trainTrigrams, trainBigrams, testTrigrams):
+    ppx=1
+    N=0
+    V=len(trainBigrams)
+    #trigramCount, bigramCount=0,0
+    for trigram in testTrigrams:
+        #find occurances of bigram in training data and adding V and 1 for laplace smoothing
+        bigram=trigram[:2]
+        try:
+            bigramCount=trainBigrams[bigram]+V
+        except (KeyError):
+            bigramCount=V
+
+        try:
+            trigramCount=trainTrigrams[trigram]+1
+        except (KeyError):
+            trigramCount=1
+
+        prTriGivenBi=trigramCount/bigramCount
+        testTriCount=testTrigrams[trigram]
+        N=N+testTriCount
+        ppx=ppx+math.log(prTriGivenBi,2)*testTriCount
+
+    return pow(2,-ppx/N)
+
+
+if __name__=='__main__':
+    #initialising variables
+    #pnCount dictionary saves the number of negative/positive words and articles
+    pnCount={'pArticles':0,'nArticles':0, 'nCount':0, 'pCount':0}
+    T,V=[],[]
+    breakFlag=0
+    i=0
+
+    #reading from file formatting it and storing it in T
+    with jsonlines.open('./signal-news1/signal-news1.jsonl', 'r') as reader:
+        for line in reader:
+            i=i+1
+            text=line['content'].lower()
+
+            #preocess text, lemmatize it and add it ot list
+            proctext=prep(text).split()
+            del text
+            lemT=lemmatize(proctext)
+            del proctext
+            T.extend(lemT)
+
+            #flag to deperate first 16000 line from test corpus
+            if(i==16000):
+                breakFlag=len(T)
+
+            #create temporary article vocabulary and pass it to pVn that counts positive vs negative words and artcles
+            tempV=collections.Counter(lemT)
+            pVn(pnCount,tempV)
+            del lemT
+
+
+    vCount=len(set(T))
+    tCount=len(T)
+    print('vocabulary size is: ',vCount,'\ntoken size is: ', tCount)
+    print('there are ',pnCount['nArticles'], 'negative articles', '\nthere are ',pnCount['pArticles'], 'positive articles')
+    print('there are ',pnCount['nCount'], 'negative words', '\nthere are ',pnCount['pCount'], 'positive words')
+
+    tri= collections.Counter(nltk.trigrams(T))
+    topTri=sorted(tri.items(), key=lambda x: x[1], reverse=True)
+    del tri
+    print('the top 25 trigrams are: \n',topTri[:25])
+    del topTri
+
+    tri1=collections.Counter(nltk.trigrams(T[:breakFlag]))
+    bi1=collections.Counter(nltk.bigrams(T[:breakFlag]))
+    tri2=collections.Counter(nltk.trigrams(T[breakFlag:]))
+    del T
+    sortedTri1=sorted(tri1.items(), key=lambda x: x[0])
+    print('the generated sentence starting with (is this) is:  ', finishSentence(sortedTri1, 'is this', 10))
+    del sortedTri1
+
+    print('the preplexity of the language model is: ', preplexity(tri1, bi1, tri2))
+    del tri1, bi1, tri2
+
+
+
+
+#
