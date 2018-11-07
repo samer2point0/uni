926dfcd997cfda6365e36aff6db294586433bc0a
samer Uni <samirsruaj@live.com>
2018-11-07 11:13:17 +0000

    uni version

--- FDA/lab2/manip.py
+++ FDA/lab2/manip.py
@@ -2,7 +2,7 @@
 
 namesList=['symboling', 'normalized-losses', 'make', 'fuel-type','aspiration', 'num-of-doors','body-style', 'drive-wheels', 'enigne-location', 'wheel-base', 'length', 'width', 'height', 'curb-weight', 'enginer-type', 'num-of-cylinders', 'engine-size', 'fuel-system','bore', 'stroke','compression-ration', 'horsepower', 'peak-rpm', 'city-mpg', 'highway-mpg','price']
 
-with open('./imports-85-data.txt') as DF:
+with open('./../imports-85-data.txt') as DF:
     DA=[namesList]
     for line in DF:
         DA.append(DF.read().replace('\n','').split(','))
