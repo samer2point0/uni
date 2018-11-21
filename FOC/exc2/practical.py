82f00187647d6bb9d22615e97fc2721cfc2d3e0d
Samer Mohamed <Samirsuraj@live.com>
2018-11-21 20:18:29 +0200

    finish FOC exc2

--- FOC/exc2/practical.py
+++ FOC/exc2/practical.py
@@ -1,5 +1,3 @@
-import itertools
-
 Code = {'.-':'a',      '-...': 'b',    '-.-.':'c',
         '-..':'d',    '.':'e',      '..-.':'f',
         '--.':'g',     '....':'h',   '..':'i',
@@ -19,14 +17,11 @@ Code = {'.-':'a',      '-...': 'b',    '-.-.':'c',
 
 def morseDecode(inputStringList):
 	"""
-	This method should take a list of strings as input. Each string is equivalent to one letter
-	(i.e. one morse code string). The entire list of strings represents a word.
-
-	This method should convert the strings from morse code into english, and return the word as a string.
-
+	This method should take a list of strings containg a morse code letter sequence
+     as input and converts each string to it's equivelent letter
 	"""
-	# Please complete this method to perform the above described function
 	word=''
+    #for each charrecter append to the word so far
 	for char in inputStringList:
 		word=''.join([word, Code[char].lower()])
 	return word
@@ -34,21 +29,20 @@ def morseDecode(inputStringList):
 
 
 def morsePartialDecode(inputStringList):
+    #initiate variables and read dictionary file
     dictionaryFileLoc ='./dictionary.txt'
     Dict=[]
-
+    lettersList=[]
     with open(dictionaryFileLoc,'r') as DF:
         for line in DF:
             Dict.append(line.strip())
 
-    Dict.sort()
-    lettersList=[]
-    #creates list of tuples with containing the possible letters for each morse code sequence
+    #creates list of tuples containing the possible letters for each morse code sequence read
     for pchar in inputStringList :
-
         tempdot=pchar.replace('x', '.')
         tempdash=pchar.replace('x', '-')
-
+        # after replacing the first signal with dot/dash check if such a letter exists
+        #and if it does append it to the letters list
         if tempdot in Code and tempdash in Code:
             lettersList.append((Code[tempdot],Code[tempdash]))
         elif tempdot in Code:
@@ -58,20 +52,23 @@ def morsePartialDecode(inputStringList):
         else:
             print('invalid char code')
 
-    print(lettersList)
-    #could also implement recursivly, complexity 2^n
+    #next part creates a list of all the possible sequences given the letters list generated
+    #this part is implemented iteratively because otherwise it could escape python's recursive depth
     tup=lettersList.pop()
+    #add the possible lastst letters as the beggining of the words list
     wordList=[tup[0],tup[1]]
     while lettersList != []:
         #staring from last letter because more efficient
         tup=lettersList.pop()
         tempList=wordList.copy()
+        """for each word concatenated in the list so far remove it and replace it with
+        one or two words depending on the next possible letter in the letters list"""
         for word in tempList:
             wordList.remove(word)
             for i in range(len(tup)):
                 wordList.append(''.join([tup[i],word]))
 
-
+    #for each word in wors list check if word is in dictionary, if not remove it
     tempList=wordList.copy()
     for word in tempList:
         if not (word in Dict):
@@ -82,31 +79,26 @@ def morsePartialDecode(inputStringList):
 
 class Maze:
     def __init__(self):
-        """
-		Constructor - You may modify this, but please do not add any extra parameters
-		"""
+        #initialise grid and a opens which is a list of the open locations
         self.grid=[[1]]
         self.opens=[]
 
     def addCoordinate(self,x,y,blockType):
-        """
-        Add information about a coordinate on the maze grid
-        x is the x coordinate
-        y is the y coordinate
-        blockType should be 0 (for an open space) of 1 (for a wall)
-        """
+        """if x or y is out of range of the self.grid variable add coulumns and
+         rows untill x,y fit, aand fill each entry with 1"""
         for j in range(len(self.grid)):
             if y >= len(self.grid):
                 self.grid.append([1 for x in range(len(self.grid[0]))])
             for i in range(x - len(self.grid[j])+1):
                 self.grid[j].append(1)
 
+        #add actuall coordinate
         self.grid[y][x]=blockType
 
 
     def printMaze(self):
         """
-        Print out an ascii representation of the maze.
+        Prints out an ascii representation of the maze.
         A * indicates a wall and a empty space indicates an open space in the maze
         """
         for y in range(len(self.grid)):
@@ -119,6 +111,7 @@ class Maze:
 
 
     def open(self):
+        """updates the variable opens containing the available spaces as a tuple of (x,y)"""
         for y in range(len(self.grid)):
             for x in range(len(self.grid[0])):
                 if self.grid[y][x]==0:
@@ -126,13 +119,19 @@ class Maze:
 
 
     def findMove(self, route,pos, goal):
+        """returns goal if it's a possible move, if not return a move that's not going backwards
+        otherwise return same position"""
+
         x,y=pos[0],pos[1]
         Move=None
+        #all possible moves filtered on whats is also an open spance still
         allmovesList=[(x-1,y),(x+1,y),(x,y-1),(x,y+1)]
         allmovesList=list(filter(lambda x: x in self.opens, allmovesList))
 
+        #find all possible moves that are not backwards and store them in choice
         choice=list(filter(lambda x: not x in route, allmovesList))
 
+        #return goal, otherwise one of the choice moves, otherwise same locations
         if goal in allmovesList:
             return goal
         elif choice:
@@ -143,31 +142,36 @@ class Maze:
 
     def findRoute(self,x1,y1,x2,y2):
         """
-        This method should find a route, traversing open spaces, from the coordinates (x1,y1) to (x2,y2)
-        It should return the list of traversed coordinates followed along this route as a list of tuples (x,y),
-        in the order in which the coordinates must be followed
-        If no route is found, return an empty list
+        This method finds a route, traversing (depth first search) open spaces, from the coordinates (x1,y1) to (x2,y2)
+        It should return the list route containing the path if no route found it returns an empty list
         """
+        #initialise first move, route , and open spaces
         move=(x1,y1)
         route=[move]
         self.open()
-        #depth first search
+
+        """depth first search is implemeted, by following a path until it's blocked and then traversing back until
+         the last open space, filling the return route as deadends on the way"""
         i=0
         while True:
             i=i+1
             x,y=move[0],move[1]
             move=self.findMove(route,(x,y),(x2,y2))
+            #if move is goal append it to route and break from loop
             if move==(x2,y2):
                 route.append(move)
                 break
+            #else if move returned is the same location, mark that location as a deadend by removing it form self.opens variables
+            #after that traverse back by taking a step backwards and removing a move from the route
             elif move==(x,y):
                 route.remove(move)
                 self.opens.remove(move)
                 move=route[-1]
+            #if move is not goal and not deadend append it to route
             else:
                 route.append(move)
 
-
+            #if the loop ran for more than the number of possible positions twice, infer no possible route and hence return empty list
             if i > 2*len(self.grid)*len(self.grid[0]):
                 print(route)
                 return []
